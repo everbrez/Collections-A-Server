5e87ac276b4e12df8fdbb64ddd09e2e3d67a669f
everbrez <everbrez@gmail.com>
2019-04-06 15:00:28 +0800

    [config] use local database instead

--- config/index.js
+++ config/index.js
@@ -1,11 +1,12 @@
 const config = {
   database: {
-    uri: 'mongodb://ds131676.mlab.com:31676/collections',
-    // uri: 'mongodb://127.0.0.1:27017/collections',
+    // mlab uri
+    // uri: 'mongodb://ds131676.mlab.com:31676/collections',
+    uri: 'mongodb://127.0.0.1:27017/collections',
     options: {
       dbName: 'collections',
-      user: 'everbrez',
-      pass: 'everbrez233',
+      // user: 'everbrez',
+      // pass: 'everbrez233',
     },
   },
   server: {
