ba94c6beb39828779be1671cb981eed461015c7a
everbrez <everbrez@gmail.com>
2019-04-15 22:49:44 +0800

    [register] err handle

--- routes/user/register.js
+++ routes/user/register.js
@@ -1,11 +1,20 @@
 import express from 'express'
 
+import User from 'models/user'
+
 const router = express.Router()
 
 router.post('/', async (req, res) => {
   const { body } = req
-  console.log('post: ', body)
-  res.json(body)
+  const { userName, password, mail } = body
+
+  const result = await User.addUser({
+    user_name: userName,
+    user_pwd: password,
+    mail,
+  }).catch(err => ({ code: 500, message: err.message }))
+
+  res.json(result)
   res.end()
 })
 
